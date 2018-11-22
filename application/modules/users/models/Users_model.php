ecc51fe52e79f273b4195d1c855d0f06eeeb76f4
Jagannath <bhulbhal1981@gmail.com>
2018-11-22 16:01:48 +0530

    changes for users login and dashboard

--- application/modules/users/models/Users_model.php
+++ application/modules/users/models/Users_model.php
@@ -5,15 +5,14 @@ class Users_model extends MY_Model {
 		parent::__construct();
 	}
 	function get_all_company_users($user_id, $company_id){
-		//$user_id = $this->session->userdata('user_id');
 		$this->db->select('*');
 		$this->db->from('users');
 		$where = "id NOT IN (1, $user_id)";
 		$this->db->where($where);
 
 		if($query = $this->db->get()) {
-			//return $query->result_array();
-			return $company_id;
+			return $query->result_array();
+			//return $company_id;
 		} else {
 			return false;
 		}
