4bb49bba57eb5a3cdaddd8c598e7c50d7dc5dcad
Jagannath <bhulbhal1981@gmail.com>
2018-11-23 14:01:03 +0530

    changes for users login and dashboard

--- admin_assets/js/praxes.js
+++ admin_assets/js/praxes.js
@@ -51,5 +51,58 @@ jQuery(document).ready(function(){
         messages: {
 
         }
+    });
+    jQuery('#edit_user_form').validate({
+        rules: {
+            username: {
+                required: true
+            },
+            email_address: {
+                required: true,
+                email: true
+            },
+            password:{
+                required: true,
+                minlength: 8
+            },
+            confirm_password:{
+                required: true,
+                minlength: 8,
+                equalTo: '#password'
+            },
+            first_name:{
+                required: true
+            },
+            last_name:{
+                required: true
+            },
+            license_numbers:{
+                required: true
+            },
+            npi_number:{
+                required: true
+            },
+            street_address:{
+                required: true
+            },
+            city:{
+                required: true
+            },
+            state:{
+                required: true
+            },
+            zip_code:{
+                required: true
+            },
+            company_id:{
+                required: true
+            },
+            user_role:{
+                required: true
+            }
+        },
+        messages: {
+
+        }
     })
 });
--- application/modules/admin/views/edit_user_view.php
+++ application/modules/admin/views/edit_user_view.php
@@ -4,17 +4,17 @@
 		<div class="col-xs-12">
 			<div class="box">
 				<div class="box-body">
-					<form id="forms" method="post" action="<?php echo base_url('admin/edit_user_form'); ?>">
+					<form id="edit_user_form" method="post" action="<?php echo base_url('admin/edit_user_form'); ?>" class="formclass">
 						<div class="row">
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>Username</label>
+									<label>Username <span>*</span></label>
 									<input type="text" name="username" class="form-control" value="<?php echo $user_data[0]['username']; ?>" readonly>
 								</div>
 							</div>
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>Email Address</label>
+									<label>Email Address <span>*</span></label>
 									<input type="text" name="email_address" class="form-control" value="<?php echo $user_data[0]['email']; ?>" readonly>
 								</div>
 							</div>
@@ -36,13 +36,13 @@
 						<div class="row">
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>First Name</label>
+									<label>First Name <span>*</span></label>
 									<input type="text" name="first_name" class="form-control" value="<?php echo $user_data[0]['first_name']; ?>">
 								</div>
 							</div>
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>Last Name</label>
+									<label>Last Name <span>*</span></label>
 									<input type="text" name="last_name" class="form-control" value="<?php echo $user_data[0]['last_name']; ?>">
 								</div>
 							</div>
@@ -56,7 +56,7 @@
 							</div>
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>License Numbers</label>
+									<label>License Numbers <span>*</span></label>
 									<input type="text" name="license_numbers" class="form-control" value="<?php echo $user_data[0]['license_numbers']; ?>">
 								</div>
 							</div>
@@ -64,13 +64,13 @@
 						<div class="row">
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>NPI Number</label>
+									<label>NPI Number <span>*</span></label>
 									<input type="text" name="npi_number" class="form-control" value="<?php echo $user_data[0]['npi_number']; ?>">
 								</div>
 							</div>
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>Street Address</label>
+									<label>Street Address <span>*</span></label>
 									<input type="text" name="street_address" class="form-control" value="<?php echo $user_data[0]['street_address']; ?>">
 								</div>
 							</div>
@@ -78,13 +78,13 @@
 						<div class="row">
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>City</label>
+									<label>City <span>*</span></label>
 									<input type="text" name="city" class="form-control" value="<?php echo $user_data[0]['city']; ?>">
 								</div>
 							</div>
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>State</label>
+									<label>State <span>*</span></label>
 									<input type="text" name="state" class="form-control" value="<?php echo $user_data[0]['state']; ?>">
 								</div>
 							</div>
@@ -92,13 +92,13 @@
 						<div class="row">
 							<div class="col-xs-6">
 								<div class="form-group">
-									<label>ZIP code</label>
+									<label>ZIP code <span>*</span></label>
 									<input type="text" name="zip_code" class="form-control" value="<?php echo $user_data[0]['zip_code']; ?>">
 								</div>
 							</div>
                             <div class="col-xs-6">
                                 <div class="form-group">
-                                    <label>Company</label>
+                                    <label>Company <span>*</span></label>
                                     <select class="form-control" name="company_id">
 	                                    <?php $company_id = $user_data[0]['company_id']; ?>
 										<?php if(!empty($companies)){ ?>
@@ -113,7 +113,7 @@
                         <div class="row">
                             <div class="col-xs-6">
                                 <div class="form-group">
-                                    <label>User Role</label>
+                                    <label>User Role <span>*</span></label>
                                     <select class="form-control" name="user_role">
 				                        <?php $role_id = $user_data[0]['user_role']; ?>
 				                        <?php if(!empty($roles)){ ?>
