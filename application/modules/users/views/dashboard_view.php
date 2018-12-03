fefbe6c4d08b0ae959f0b10e6797e20222e5e66c
Jagannath <bhulbhal1981@gmail.com>
2018-12-03 16:16:16 +0530

    changes for users login and dashboard

--- application/modules/users/views/dashboard_view.php
+++ application/modules/users/views/dashboard_view.php
@@ -1,8 +1,14 @@
 <!-- Main content -->
-<section class="content">
+<!--<section class="content">
     <div class="callout callout-success">
         <h4>Welcome!</h4>
+        <p>Welcome to Praxes! You are logged in as <?php /*echo $this->session->userdata('first_name').' '.$this->session->userdata('last_name'); */?>. </p>
+    </div>
+</section>-->
+<div class="user_contentainer">
+    <div class="user_contentainer_title">Welcome!</div>
+    <div class="user_contentainer_content">
         <p>Welcome to Praxes! You are logged in as <?php echo $this->session->userdata('first_name').' '.$this->session->userdata('last_name'); ?>. </p>
     </div>
-</section>
+</div>
 <!-- /.content -->
