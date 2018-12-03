634fb553b9739b1a008cee63f9f67b28a71a595f
Jagannath <bhulbhal1981@gmail.com>
2018-12-03 13:44:50 +0530

    changes for users login and dashboard

--- application/modules/users/views/main_template_view.php
+++ application/modules/users/views/main_template_view.php
@@ -1,158 +1,76 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Users | <?php echo $html_title; ?></title>
-	<!-- Tell the browser to be responsive to screen width -->
-	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-	<!-- Bootstrap 3.3.7 -->
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/bootstrap.min.css'); ?>">
-	<!-- Font Awesome -->
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/font-awesome.min.css'); ?>">
-	<!-- Ionicons -->
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/ionicons.min.css'); ?>">
-	<!-- jvectormap -->
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/jquery-jvectormap.css'); ?>">
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/footable.bootstrap.min.css'); ?>">
-	<!-- Theme style -->
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/AdminLTE.min.css'); ?>">
-	<!-- AdminLTE Skins. Choose a skin from the css/skins
-		 folder instead of downloading all of them to reduce the load. -->
-	<link rel="stylesheet" href="<?php echo base_url('admin_assets/css/_all-skins.min.css'); ?>">
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-grid.min.css'); ?>">
+    <link href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
 
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-
-	<!-- Google Font -->
-	<link rel="stylesheet"
-	      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
-
-	<header class="main-header">
-
-		<!-- Logo -->
-		<a href="javascript:void(0)" class="logo">
-			<!-- mini logo for sidebar mini 50x50 pixels -->
-			<span class="logo-mini"><b>P</b></span>
-			<!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Praxes</b></span>
-		</a>
-
-		<!-- Header Navbar: style can be found in header.less -->
-		<nav class="navbar navbar-static-top">
-			<!-- Navbar Right Menu -->
-			<div class="navbar-custom-menu">
-				<ul class="nav navbar-nav">
-					<!-- User Account: style can be found in dropdown.less -->
-					<li class="dropdown user user-menu">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<img src="<?php echo base_url('admin_assets/images/user2-160x160.jpg'); ?>" class="user-image" alt="User Image">
-							<span class="hidden-xs"><?php echo $username; ?></span>
-						</a>
-						<ul class="dropdown-menu">
-							<!-- User image -->
-							<li class="user-header">
-								<img src="<?php echo base_url('admin_assets/images/user2-160x160.jpg'); ?>" class="img-circle" alt="User Image">
-
-								<p><?php echo $this->session->userdata('first_name').' '.$this->session->userdata('last_name'); ?> - <?php echo $username; ?></p>
-							</li>
-							<!-- Menu Footer-->
-							<li class="user-footer">
-								<div class="pull-left">
-									<a href="#" class="btn btn-default btn-flat">Profile</a>
-								</div>
-								<div class="pull-right">
-									<a href="<?php echo base_url('users/logout'); ?>" class="btn btn-default btn-flat">Sign out</a>
-								</div>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-
-		</nav>
-	</header>
-	<!-- Left side column. contains the logo and sidebar -->
-	<aside class="main-sidebar">
-		<!-- sidebar: style can be found in sidebar.less -->
-		<section class="sidebar">
-			<!-- Sidebar user panel -->
-			<div class="user-panel">
-				<div class="pull-left image">
-					<img src="<?php echo base_url('admin_assets/images/user2-160x160.jpg'); ?>" class="img-circle" alt="User Image">
-				</div>
-				<div class="pull-left info">
-					<p><?php echo $username; ?></p>
-					<a href="javascript:void(0);"><i class="fa fa-circle text-success"></i> Online</a>
-				</div>
-			</div>
-			<!-- sidebar menu: : style can be found in sidebar.less -->
-			<ul class="sidebar-menu" data-widget="tree">
-				<li class="header">MAIN NAVIGATION</li>
-                <li><a href="<?php echo base_url('users/dashboard'); ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-                <li><a href="<?php echo base_url('users/manage-staff'); ?>"><i class="fa fa-users"></i> Manage Staff</a></li>
-			</ul>
-		</section>
-		<!-- /.sidebar -->
-	</aside>
-
-	<!-- Content Wrapper. Contains page content -->
-	<div class="content-wrapper">
-		<!-- Content Header (Page header) -->
-		<section class="content-header">
-			<h1><?php echo $page_title; ?></h1>
-			<ol class="breadcrumb">
-				<li><a href="<?php echo base_url('users/'); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
-				<li class="active"><?php echo $page_title; ?></li>
-			</ol>
-		</section>
-		<!-- Main content -->
-		<?php $this->load->view($content_view); ?>
-		<!-- /.content -->
-	</div>
-	<!-- /.content-wrapper -->
-
-	<footer class="main-footer">Copyright &copy; <?php echo date('Y'); ?> All rights reserved.</footer>
-
-	<!-- Add the sidebar's background. This div must be placed
-		 immediately after the control sidebar -->
-	<div class="control-sidebar-bg"></div>
+<body>
+<div id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="logo">
+                    <a href="#"><img src="<?php echo base_url('assets/images/logo.jpg'); ?>"/></a>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div class="mainnav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Faq’s</a></li>
+                        <li><a href="#">Providers</a></li>
+                        <li><a href="#">Organizations</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-1">
+                <div class="login">
+                    <a href="<?php echo base_url('login'); ?>">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<!-- ./wrapper -->
-
-<!-- jQuery 3 -->
-<script src="<?php echo base_url('admin_assets/js/jquery.min.js'); ?>"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?php echo base_url('admin_assets/js/bootstrap.min.js'); ?>"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url('admin_assets/js/fastclick.js'); ?>"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo base_url('admin_assets/js/adminlte.min.js'); ?>"></script>
-<!-- Sparkline -->
-<script src="<?php echo base_url('admin_assets/js/jquery.sparkline.min.js'); ?>"></script>
-<!-- jvectormap  -->
-<script src="<?php echo base_url('admin_assets/js/jquery-jvectormap-1.2.2.min.js'); ?>"></script>
-<script src="<?php echo base_url('admin_assets/js/jquery-jvectormap-world-mill-en.js'); ?>"></script>
-<!-- SlimScroll -->
-<script src="<?php echo base_url('admin_assets/js/jquery.slimscroll.min.js'); ?>"></script>
-<!-- ChartJS -->
-<!--<script src="<?php /*echo base_url('admin_assets/js/Chart.js'); */?>"></script>-->
-<script src="<?php echo base_url('admin_assets/js/footable.min.js'); ?>"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url('admin_assets/js/dashboard2.js'); ?>"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url('admin_assets/js/demo.js'); ?>"></script>
-<!--<script>
-    jQuery(document).ready(function(){
-        jQuery('#footable').footable();
-    })
-</script>-->
+<?php $this->load->view($content_view); ?>
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <h4>Menu</h4>
+                <div class="footer_menu">
+                    <ul>
+                        <li><a href="#">Praxes</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Request Demo</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <h4>Log In</h4>
+            </div>
+            <div class="col-lg-4">
+                <p>&copy; 2018, PRAXES<br/>Terms of Use  I  Privacy Policy</p>
+                <h4>Connect with us</h4>
+                <ul class="footer_social_link">
+                    <li><a target="_blank" href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a target="_blank" href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a target="_blank" href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li><a target="_blank" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
+<script type="application/javascript" src="//code.jquery.com/jquery-3.3.1.min.js" />
+<script type="application/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>" />
 </html>
