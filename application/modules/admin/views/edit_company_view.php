4e7ed3f298458813c6a02e4e7e8e75478a7084ab
Jagannath <bhulbhal1981@gmail.com>
2018-12-03 13:19:44 +0530

    changes for users login and dashboard

--- application/modules/admin/views/edit_company_view.php
+++ application/modules/admin/views/edit_company_view.php
@@ -9,7 +9,7 @@
 							<div class="col-xs-10">
 								<div class="form-group">
 									<label>Company <span>*</span></label>
-									<input type="text" name="company_name" class="form-control" value="">
+									<input type="text" name="company_name" class="form-control" value="<?php echo $company_name; ?>">
 								</div>
 							</div>
 							<div class="col-xs-2">
